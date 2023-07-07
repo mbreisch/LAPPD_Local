fba68a3a53d5f04065f7b7189e716c8e466e25a9
mbreisch <64076923+mbreisch@users.noreply.github.com>
2023-07-07 17:19:55 +0200

    1.6.9

--- DataModel/ACC.cpp
+++ DataModel/ACC.cpp
@@ -583,6 +583,8 @@ int ACC::listenForAcdcData(int trigMode, vector<int> LAPPD_on_ACC)
 		{
 			if(lastAccBuffer.at(14) & (1 << k))
 			{
+                unsigned int bit = lastAccBuffer.at(14) & (1 << k);
+                std::cout << k << ": " << bit << " - " << lastAccBuffer.at(16+k) << std::endl;
 				if(lastAccBuffer.at(16+k)==PSECFRAME)
 				{
 					boardsReadyForRead.push_back(k);
